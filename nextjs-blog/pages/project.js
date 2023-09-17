87304448acfa2d3f965e2d40353469f6d047d006
Haokai Xu <xuhaokai.us@gmail.com>
2023-09-16 19:37:07 -0700

    tester table filter

--- nextjs-blog/pages/project.js
+++ nextjs-blog/pages/project.js
@@ -1,13 +1,14 @@
 // Project.js
 
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const Project = () => {
   const router = useRouter();
   const { projectName } = router.query; // Get the project name from the URL query
 
   // Sample test user data (you can replace this with your own data)
-  const testUsers = [
+  const initialTestUsers = [
     {
       id: 1,
       name: 'John Doe',
@@ -31,11 +32,102 @@ const Project = () => {
     // Add more test users here...
   ];
 
+  const [testUsers, setTestUsers] = useState(initialTestUsers);
+  const [filters, setFilters] = useState({
+    name: '',
+    email: '',
+    age: '',
+    location: '',
+    decideType: '',
+    technicalProficiency: '',
+    gender: '',
+  });
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
+    filterData(filters);
+  };
+
+  const filterData = (filterValues) => {
+    const filteredUsers = initialTestUsers.filter((user) =>
+      Object.entries(filterValues).every(([key, value]) =>
+        value === '' || String(user[key]).toLowerCase().includes(value.toLowerCase())
+      )
+    );
+    setTestUsers(filteredUsers);
+  };
+
   return (
     <div>
       <h1>Project: {projectName}</h1>
 
       <h2>Test Users</h2>
+      <div className="filter-inputs">
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={filters.name}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            name="email"
+            value={filters.email}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Age:</label>
+          <input
+            type="text"
+            name="age"
+            value={filters.age}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Location:</label>
+          <input
+            type="text"
+            name="location"
+            value={filters.location}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Decide Type:</label>
+          <input
+            type="text"
+            name="decideType"
+            value={filters.decideType}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Tech Proficiency:</label>
+          <input
+            type="text"
+            name="technicalProficiency"
+            value={filters.technicalProficiency}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
+          <label>Gender:</label>
+          <input
+            type="text"
+            name="gender"
+            value={filters.gender}
+            onChange={handleFilterChange}
+          />
+        </div>
+      </div>
       <div className="table-container">
         <table className="user-table">
           <thead>
@@ -68,6 +160,20 @@ const Project = () => {
       </div>
 
       <style jsx>{`
+        .filter-inputs {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        .filter-inputs div {
+          margin-right: 10px;
+          margin-bottom: 10px;
+        }
+
+        .filter-inputs label {
+          display: block;
+        }
+
         .table-container {
           max-width: 100%;
           overflow-x: auto;
